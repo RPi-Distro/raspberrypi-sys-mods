af5efe98c56a0947614493e7ade4d36e4f840d55
Simon Long <simon@raspberrypi.org>
2016-12-05 13:45:47 +0000

    Improve robustness of SSH password checks

--- etc/profile.d/sshpasswd.sh
+++ etc/profile.d/sshpasswd.sh
@@ -1,17 +1,25 @@
 check_hash ()
 {
-	local SHADOW="$(sudo -n grep -E '^pi:' /etc/shadow 2>/dev/null)"
-	test -n "${SHADOW}" || return 0
-	local SALT=$(echo "${SHADOW}" | sed -n 's/pi:\$6\$//;s/\$.*//p')
-	local HASH=$(mkpasswd -msha-512 raspberry "$SALT")
+   if grep -q "^PasswordAuthentication\s*no" /etc/ssh/sshd_config ; then return 0 ; fi
+   test -x /usr/bin/mkpasswd || return 0
+   SHADOW="$(sudo -n grep -E '^pi:' /etc/shadow 2>/dev/null)"
+   test -n "${SHADOW}" || return 0
+   if echo $SHADOW | grep -q "pi:!" ; then return 0 ; fi
+   SALT=$(echo "${SHADOW}" | sed -n 's/pi:\$6\$//;s/\$.*//p')
+   HASH=$(mkpasswd -msha-512 raspberry "$SALT")
+   test -n "${HASH}" || return 0
 
-	if systemctl is-active ssh -q && echo "${SHADOW}" | grep -q "${HASH}"; then
+   if echo "${SHADOW}" | grep -q "${HASH}"; then
 		echo
 		echo "SSH is enabled and the default password for the 'pi' user has not been changed."
 		echo "This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password."
 		echo
-	fi
+   fi
 }
 
-check_hash
-unset check_hash
+if service ssh status | grep -q running; then
+	check_hash
+	unset check_hash
+fi
+
+
